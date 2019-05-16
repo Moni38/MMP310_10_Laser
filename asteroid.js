3cc300fcff004a208a93ddf07328eda97ee733cf
Moni38 <42787726+Moni38@users.noreply.github.com>
2019-05-16 14:21:43 -0400

    Update asteroid.js

--- asteroid.js
+++ asteroid.js
@@ -1,41 +1,39 @@
 /*
 	asteroid class
 */
+var asteroids = [];
 
 class Asteroid {
-	constructor() {
-		this.x = random(width);
-		this.y = -100;
-		this.size = random (65,50);
-		this.speed = {
-			x: random(-1, 1),
-			y: random(5)
-		};
+    constructor() {
+        this.x = random(width);
+        this.y = -100;
+        this.size = random(65, 50);
+        this.speed = {
+            x: random(-1, 1),
+            y: random(5)
+        };
         this.color = ('yellow');
-	}
-	
-	display() {
-		fill(this.color);
-		noStroke();
-		ellipse(this.x, this.y, this.size);
-	}
-	
-	update() {
-		this.x += this.speed.x;
-		this.y += this.speed.y;
-	}
-	
-	collide() {
-		var d = dist(this.x, this.y, spaceship.x, spaceship.y);
-		if (d < this.size / 2) {
-			textSize(80);
-			textAlign(CENTER, CENTER);
-			fill('red');
-			text("GAME OVER", width/2, height/2);
-			noLoop();	
-		}
-	}
+    }
+
+    display() {
+        fill(this.color);
+        noStroke();
+        ellipse(this.x, this.y, this.size);
+    }
+
+    update() {
+        this.x += this.speed.x;
+        this.y += this.speed.y;
+    }
+
+    collide() {
+        var d = dist(this.x, this.y, spaceship.x, spaceship.y);
+        if (d < this.size / 2) {
+            textSize(80);
+            textAlign(CENTER, CENTER);
+            fill('red');
+            text("GAME OVER", width / 2, height / 2);
+            noLoop();
+        }
+    }
 }
-
-
-
